3ec2360e91d51701f3d11124e4d1066c3e9414d2
Liza <el.dubogryz@gmail.com>
2020-10-30 13:21:42 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+window.addEventListener('scroll', function() {
+  let scrollValue = pageYOffset;
+  let button = document.querySelector('.listen-button');
+  if(scrollValue<=400){
+  	button.style.backgroundColor = 'white';
+  }
+  else {
+  	button.style.backgroundColor = '#14d8cc';
+  }
+});
+let slidebar = document.querySelector('.slidebar');
+
+let btSlidebar = document.querySelector('.menu-img');
+btSlidebar.onclick = function() {
+	slidebar.style.visibility = 'visible';
+	slidebar.style.width = '270px';
+	slidebar.className = 'increasebleSlidebar';
+	
+	
+	let page = document.querySelector('body');
+	page.onclick = function f1 () {
+		if(event.pageX>270){
+			//slidebar.style.visibility = 'hidden';
+			//slidebar.style.width = '0';
+			slidebar.addEventListener("animationend", function f2() {
+				slidebar.style.visibility = 'hidden';
+				slidebar.style.width = '0';
+				slidebar.className = 'slidebar';
+				slidebar.removeEventListener('animationend', f2);
+			});
+
+			slidebar.className = 'reducebleSlidebar';
+			//if(slidebar.type === 'animationend'){
+				//slidebar.className = 'slidebar';
+			//}
+			body.removeEventListener('click', f1);
+			
+		}
+	}
+	
+};
+
+
+
